74937e07f132704ef59cbdc0b759a8dd4135433e
Dmitry Holodov <dimalinux@protonmail.com>
2024-03-11 15:57:07 -0700

    fixed size issue

--- src/client/main.rs
+++ src/client/main.rs
@@ -3,6 +3,8 @@ use std::net::UdpSocket;
 const SERVER_ADDR: &str = "127.0.0.1:2048";
 const CLIENT_ADDR: &str = "127.0.0.1:0"; // random port
 
+const MAX_BUF_SZ: usize = 2048;
+
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     let mut message = "Default message";
@@ -16,9 +18,17 @@ fn main() -> std::io::Result<()> {
     let message_sz = socket.send_to(message.as_bytes(), SERVER_ADDR)?;
     println!("\nsent echo of {} bytes to server\n", message_sz);
 
-    let mut buf = [0; 2048];
-    let (amt, src) = socket.recv_from(&mut buf)?;
+    let mut buf = [0; MAX_BUF_SZ];
+    let (amt, src) = match socket.recv_from(&mut buf) {
+        Ok(result) => result,
+        Err(err) => {
+            // Handle the error here
+            eprintln!("Error receiving data from socket: {}\n", err);
+            return Err(err); // Or take some other recovery action
+        },
+    };
     let echo = std::str::from_utf8(&buf[..amt]).unwrap();
     println!("Echo from: {:?}, size: {:?}\n{}\n", src, amt, echo);
     Ok(())
 }
+
--- src/server/main.rs
+++ src/server/main.rs
@@ -3,17 +3,39 @@ use std::str;
 
 const LISTEN_ADDR: &str = "127.0.0.1:2048";
 
-fn main() -> std::io::Result<()> {
-    // TODO: Better error handling
-    let socket = UdpSocket::bind(LISTEN_ADDR.to_string())?;
-    println!("\nstart server\n");
+fn event_loop(socket:UdpSocket) -> std::io::Result<()> {
+    let mut buf = [0; 2048];
 
     loop {
-        let mut buf = [0; 2048];
-        let (amt, src) = socket.recv_from(&mut buf)?;
-        let message = str::from_utf8(&buf[0..amt]).unwrap();
-        println!("From: {:?}, buf: {:?}", src, message);
+        let (amt, src) = match socket.recv_from(&mut buf) {
+            Ok(result) => result,
+            Err(err) => {
+                // Handle the error here
+                eprintln!("Error receiving data from socket: {}\n", err);
+                return Err(err); // Or take some other recovery action
+            }
+        };
 
-        socket.send_to(&buf, src)?;
+
+        let message_buf = &buf[0..amt];
+        let message = str::from_utf8(message_buf).unwrap();
+        println!("From: {:?}, size={} message:\n{:?}", src, amt, message);
+        let amt = socket.send_to(message_buf, src)?;
+        println!("sent {} bytes", amt)
     }
 }
+
+
+fn main() -> std::io::Result<()> {
+    let socket = match UdpSocket::bind(LISTEN_ADDR.to_string()) {
+        Ok(result) => result,
+        Err(err) => {
+            eprintln!("Error binding socket: {}\n", err);
+            return Err(err); // Or take some other recovery action
+        }
+    };
+    println!("\nstart server\n");
+
+    event_loop(socket)
+}
+
